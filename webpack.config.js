9c64e7d3badda4befb68637b4212d8e21726d38e
Daisy Smith <daisymarie128@gmail.com>
2017-02-28 18:04:54 +0000

    update to webpack2 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 	inject: 'body',
-	template: __dirname + "/app/index.html",
+	template: `${__dirname}/app/index.html`,
 	output: 'index.html'
 });
 
 module.exports = {
 	entry: [
-		'./app/index.js', './app/main.scss'
-	],
-	output: {
-		path: __dirname + '/dist',
-		filename: 'bundle.js'
-	},
+	 './app/index.js',
+	 './app/main.scss'
+ ],
+ output: {
+  path: `${__dirname}/dist`,
+  filename: 'bundle.js'
+ },
 	module: {
-		loaders: [
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				loaders: ["style-loader", "css-loader", "sass-loader"]
-			}
-		]
-	},
-	plugins: [HTMLWebpackPluginConfig]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
+  },
+	plugins: [
+		HTMLWebpackPluginConfig
+	]
 };
